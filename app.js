5b3d8e1f5f16f99cdf4e2bad0db8fb8e70a48093
keshavsoft <nknnkumar@yahoo.com>
2025-06-21 17:19:53 +0530

    app public case

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 import { router as routerFromV1 } from "./V1/routes.js";
 import { router as routerFromV2 } from "./V2/routes.js";
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.use("/V1", routerFromV1);
 app.use("/V2", routerFromV2);
