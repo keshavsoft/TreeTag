ca23bd344ea2c235309c1b87ed9ee9a1de2f8b09
keshavsoft <nknnkumar@yahoo.com>
2025-06-29 15:38:59 +0530

    gps v3 started

---
+++ Public/Gps/V3/Index/AddListeners/DOMContentLoaded/SaveButtonId/ButtonClickFunc.js
@@ -0,0 +1,55 @@
+import { StartFunc as StartFuncFromFetchAsPost } from "./FetchAsPost/entryFile.js";
+
+let StartFunc = () => {
+    // Show success message in h2 immediately
+    // const h2 = document.querySelector('h2');
+    // if (h2) {
+    //     const originalText = h2.textContent;
+    //     h2.textContent = 'Saved successfully!';
+    //     h2.classList.add('success-message');
+    //     setTimeout(() => {
+    //         h2.textContent = originalText;
+    //         h2.classList.remove('success-message');
+    //         // Clear the input field after showing the message
+    //         const userNameInput = document.getElementById('UserNameId');
+    //         if (userNameInput) userNameInput.value = '';
+    //     }, 1000);
+
+
+    // }
+
+    if (!navigator.geolocation) {
+        console.log("Geolocation not supported.");
+        return;
+    };
+
+    navigator.geolocation.getCurrentPosition(
+        jFLocalSuccess,
+        (err) => {
+            console.log("err : ", err.message);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 15000,
+            maximumAge: 0
+        }
+    );
+};
+
+const jFLocalSuccess = (pos) => {
+    const { latitude, longitude, accuracy } = pos.coords;
+    console.log("latitude : ", latitude);
+    console.log("longitude : ", longitude);
+    console.log("accuracy : ", accuracy);
+
+    StartFuncFromFetchAsPost({
+        inLatitude: latitude,
+        inLongitude: longitude,
+        inTreeName: accuracy
+    }).then(PromiseData => {
+        console.log("aaaaaaaaaaaaaa : ", PromiseData);
+
+    });
+};
+
+export { StartFunc };
