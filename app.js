9eee9fa09f40969001d173495de2abfec1df464e
keshavsoft <nknnkumar@yahoo.com>
2025-07-13 08:04:29 +0530

    index.html perfect

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
-const port = 3000;
+var port = normalizePort(process.env.PORT || 3000);
 
 import { StartFunc as StartFuncFromMiddleware } from "./Token/MiddleWares/entryFile.js";
 
@@ -11,13 +14,27 @@ import { router as routerFromV1 } from "./V1/routes.js";
 import { router as routerFromSV1 } from "./SV1/routes.js";
 import { router as routerFromV8 } from "./V8/routes.js";
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 app.use(cookieParser());
 
 app.use("/Token", routerFromToken);
 app.use("/V1", routerFromV1);
 app.use("/SV1", StartFuncFromMiddleware, routerFromSV1);
 app.use("/V8", routerFromV8);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+};
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
