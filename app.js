1f965b034e6283f84027c64e6d9d0759c0782d54
keshavsoft <nknnkumar@yahoo.com>
2025-07-18 18:00:12 +0530

    latest ui working

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-var port = normalizePort(process.env.PORT || 3000);
+var port = normalizePort(process.env.PORT || 5015);
 
 import { StartFunc as StartFuncFromMiddleware } from "./Token/MiddleWares/entryFile.js";
 
@@ -15,6 +15,8 @@ import { router as routerFromSV1 } from "./SV1/routes.js";
 import { router as routerFromV8 } from "./V8/routes.js";
 import { router as routerFromV9 } from "./V9/routes.js";
 import { router as routerFromSV2 } from "./SV2/routes.js";
+import { router as routerFromV10 } from "./V10/routes.js";
+import { router as routerFromSV3 } from "./SV3/routes.js";
 
 app.use(express.static('Public'));
 app.use(cookieParser());
@@ -25,6 +27,8 @@ app.use("/SV1", StartFuncFromMiddleware, routerFromSV1);
 app.use("/V8", routerFromV8);
 app.use("/V9", routerFromV9);
 app.use("/SV2", StartFuncFromMiddleware, routerFromSV2);
+app.use("/V10", routerFromV10);
+app.use("/SV3", StartFuncFromMiddleware, routerFromSV3);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
